71ef596f7afebfc791d56d37dcd5d0a58af117bc
Khoa <ktran7@stevens.edu>
2023-06-19 15:34:41 -0400

    changes to color scheme and contact page

--- lit/src/components/common/contact.tsx
+++ lit/src/components/common/contact.tsx
@@ -1,27 +1,38 @@
+import { ErrorResponse } from "@remix-run/router";
 import React from "react";
+import {useForm, SubmitHandler} from "react-hook-form";
+
+type Inputs = {
+    name : string
+    email : string
+    message : string
+}
 
 const Contact: React.FC = () => {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState : {errors},
+    } = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+    console.log(watch("email"))
   return (
-    <div className="contact-page">
-        <h1>Contact Us</h1>
-        <p className="contact-page__description">
-        Please fill out the form below to get in touch with us. We will respond
-        to your message as soon as possible.
-        </p>
-        <form className="contact-page__form">
-        <div className="form-group">
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" className="form-input" />
-        </div>
-        <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" className="form-input" />
-        </div>
-        <div className="form-group">
-            <label htmlFor="message">Message:</label>
-            <textarea id="message" name="message" rows={5} className="form-input" />
-        </div>
-        <button type="submit" className="submit-button">Submit</button>
+    <div className="flex mt-100 flex-wrap justify-center items-center">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex mt-20 ">
+            <div>
+                <input className="flex-grow mr-2 px-4 py-2 border border-gray-300 rounded-md" placeholder="Name" {...register("name", {required: "Please enter a name.", minLength : {value:2, message: "Name is too short."}})}/>
+                <p>{errors.name?.message}</p>
+            </div>
+            <div>
+                <input className="flex-grow mr-2 px-4 py-2 border border-gray-300 rounded-md" placeholder="Email" {...register("email", {required: "Please enter an email"})}/>
+                <p>{errors.email?.message}</p>
+            </div>
+            <div>
+                <input className="flex-grow mr-2 px-4 py-2 border border-gray-300 rounded-md" placeholder="Message" {...register("message", {required: "Please enter a message", minLength: {value: 4, message: "Message is too short."}})}/>
+                <p>{errors.message?.message}</p>
+            </div>
+            <input className="px-4 py-2 bg-black text-white rounded-md" type="submit"/>
         </form>
     </div>
   );
--- lit/src/components/common/navbar.tsx
+++ lit/src/components/common/navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "../../LITLOGO.png";
 const NavBar: React.FC = () => {
   return (
-    <nav className="bg-black">
+    <nav className="bg-black border-b-4 border-teal-500">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0 flex">
@@ -10,7 +10,7 @@ const NavBar: React.FC = () => {
 
             <a
               href="/"
-              className="nav-link text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+              className="nav-link text-white hover:text-teal-500 px-3 py-2 rounded-md text-30 font-medium"
             >
               LIT
             </a>
@@ -18,19 +18,19 @@ const NavBar: React.FC = () => {
           <div className="flex items-center">
             <a
               href="/"
-              className="nav-link text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+              className="nav-link text-white hover:text-teal-500 px-3 py-2 rounded-md text-25 font-medium"
             >
               Home
             </a>
             <a
               href="/about"
-              className="nav-link text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+              className="nav-link text-white hover:text-teal-500 px-3 py-2 rounded-md text-25 font-medium"
             >
               About
             </a>
             <a
               href="/contact"
-              className="nav-link text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+              className="nav-link text-white hover:text-teal-500 px-3 py-2 rounded-md text-25 font-medium"
             >
               Contact
             </a>
